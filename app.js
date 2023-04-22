216e80ba6aef320918bd12aae2912139a4edbf45
iamvalenciia <val22018@byui.edu>
2023-04-21 21:48:16 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import { createServer } from 'http';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Juan Pablo Valencia');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+   
