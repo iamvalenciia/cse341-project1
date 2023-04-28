efd52bf0eff6dd1ccc4776d90372eac32c3e6c93
iamvalenciia <val22018@byui.edu>
2023-04-28 16:55:21 -0500

    lesson1 changes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//This line imports the Express module
 const express = require('express');
+//This line creates a new instance of the Express application
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send("Juan Pablo Valencia");
-});
+const port = 3000;
 
-app.listen(process.env.port || 3000);
-console.log('Web Server is listening at port '+ (process.env.port || 3000));
+app.use('/', require('./routes'));
+
+//This line tells the server to start listening for incoming requests
+app.listen(process.env.port || port);
+
+console.log('http://localhost:' + (process.env.port || port) + '/');
