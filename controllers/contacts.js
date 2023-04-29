7ebdc87a871ff460ffffc9157e25ef40eeec0c78
iamvalenciia <val22018@byui.edu>
2023-04-28 20:04:52 -0500

    adding the routes to the file called "routes.rest"

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,30 +2,20 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 
-// const getAll = async (req, res, next) => {
-//   console.log('Attempting to retrieve data from "contacts" collection...');
-//   try {
-//     const result = await mongodb.getDb().db().collection('contacts').find();
-//     console.log('Successfully retrieved data from "contacts" collection.');
-//     result.toArray().then((lists) => {
-//       res.setHeader('Content-Type', 'application/json');
-//       res.status(200).json(lists);
-//     });
-//   } catch (err) {
-//     console.error('Error retrieving data from "contacts" collection:', err);
-//     next(err);
-//   }
-// };
-
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  console.log(result); // log the result here
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  console.log('Attempting to retrieve data from "contacts" collection...');
+  try {
+    const result = await mongodb.getDb().db().collection('contacts').find();
+    console.log('Successfully retrieved data from "contacts" collection.');
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  } catch (err) {
+    console.error('Error retrieving data from "contacts" collection:', err);
+    next(err);
+  }
 };
-
 
 const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
