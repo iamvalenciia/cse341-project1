221591cf4d6b9d3bf664d77af206c0d838123260
iamvalenciia <val22018@byui.edu>
2023-05-13 02:19:05 -0500

    to allow requests from any website

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
   })
   .use('/', allRoutes);
 
