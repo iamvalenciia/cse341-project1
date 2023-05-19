0c3ac55250c651154f394e12556b4cb5f9315978
iamvalenciia <val22018@byui.edu>
2023-05-18 19:34:57 -0500

    polish the api x4

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,10 +3,10 @@ import contactsController from '../controllers/contacts.js';
 
 const router = Router();
 
-router.get('/', contactsController.getAll); // http://localhost:3000/contacts/
-router.get('/:id', contactsController.getSingle); // http://localhost:3000/contacts/:id
-router.post('/create', contactsController.createContact); // http://localhost:3000/contacts/create
-router.put('/update/:id', contactsController.updateContact); // http://localhost:3000/contacts/update/:id
-router.delete('/delete/:id', contactsController.deleteContact); // http://localhost:3000/contacts/delete/:id
+router.get('/', contactsController.getAll);
+router.get('/:id', contactsController.getSingle);
+router.post('/', contactsController.createContact);
+router.put('/update/:id', contactsController.updateContact);
+router.delete('/delete/:id', contactsController.deleteContact);
 
 export default router;
