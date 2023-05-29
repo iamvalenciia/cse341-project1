6f308799ad3923c14ba3afa700767ea37e0394d8
iamvalenciia <val22018@byui.edu>
2023-05-29 00:39:05 -0500

    preparing to use Typescript

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
